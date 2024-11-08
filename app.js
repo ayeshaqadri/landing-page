1595eb3306cefde3bc4966a0b2359ca7edd538a8
ayeshaqadri <qadria392@gmail.com>
2024-11-08 18:07:43 +0500

    complete

---
+++ app.js
@@ -0,0 +1,9 @@
+// Change header style when scrolling
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
